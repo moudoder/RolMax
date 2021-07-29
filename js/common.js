09b74b2908ab5eaf1683afd5531c20f52ba37a5a
moudoder <39887984+moudoder@users.noreply.github.com>
2021-07-29 09:59:18 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -13,8 +13,22 @@ $(document).ready(function () {
 	  swipe: false,
 	  prevArrow: $('.qwiz-arrows__back'),
 	  nextArrow: $('.qwiz-arrows__next'),
+    adaptiveHeight: true,
+    asNavFor: '.qwiz-text__slider'
    });	
-
+  $('.qwiz-text__slider').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    swipe: false,
+    asNavFor: '.qwiz-slider__inner'
+   });  
+  $(".qwiz-slider__inner").on("afterChange", function (){
+     if ($('.qwiz-arrows__next').hasClass('slick-disabled')) {
+      $('.qwiz-arrows__back').addClass('slick-disabled');
+     }
+  })
    $('.qwiz-block').on('click', function() {
    	let parent = $(this).parent('.qwiz-row');
    	let childrens = $(parent).children('.qwiz-block');
@@ -34,4 +48,18 @@ $(document).ready(function () {
    	$('.modal-window').removeClass('modal-window-active');
    	return false;
    })
+   $('.qwiz-range_result').text($('.rng-1').val());
+   $('.rng-1').rangeslider({
+       polyfill : false,
+       onSlide : function( position, value ) {
+          $('.qwiz-range_result').text(value);
+       },
+   });
+   $('.rng-2').text($('.rst-2').val());
+   $('.rng-2').rangeslider({
+       polyfill : false,
+       onSlide : function( position, value ) {
+          $('.rst-2').text(value);
+       },
+   });
 })
